3634cd44d1a728aeffd85cfccb00d5856058cc31
brlathanjr <brlathanjr@gmail.com>
2024-12-05 02:02:02 -0600

    Check in the additional files for VPN Sidecar

---
+++ vpn-sidecar/root/entrypoint.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# create tun device
+if [ ! -c /dev/net/tun ]; then
+  mkdir -p /dev/net
+  mknod /dev/net/tun c 10 200
+fi
+
+# Enable devices MASQUERADE mode
+iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+
+# start vpn client
+openvpn --config /vpn/config/${VPN_CONFIG}
